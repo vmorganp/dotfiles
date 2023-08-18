b5c36cfe9dd6c13e94e702da4231b05128ae2546
vmorganp <31448722+vmorganp@users.noreply.github.com>
2023-08-18 13:16:59 -0700

    twm hell

---
+++ i3/README.md
@@ -0,0 +1,9 @@
+requires installation of
+
+1. rofi
+1. polybar
+1. i3
+1. whatever dependenies i3 has
+1. playerctl
+1. nitrogen
+
---
+++ i3/polybar.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# shamelessly yoinked from
+# https://www.reddit.com/r/i3wm/comments/6lo0z0/how_to_use_polybar/
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
