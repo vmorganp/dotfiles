8158a4c5a3c21026fc5e217c2e91400c50798309
vmorganp <31448722+vmorganp@users.noreply.github.com>
2023-09-04 03:58:16 -0700

    vim plugin updates

--- kickStartNvim/.config/nvim/lua/custom/plugins/flash.lua
+++ kickStartNvim/.config/nvim/lua/custom/plugins/flash.lua
@@ -1,15 +1,15 @@
-return {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {},
-    keys = {
-        {
-            "s",
-            mode = { "n", "x", "o" },
-            function()
-                require("flash").jump()
-            end,
-            desc = "Flash",
-        },
-    },
-}
+-- return {
+--     "folke/flash.nvim",
+--     event = "VeryLazy",
+--     opts = {},
+--     keys = {
+--         {
+--             "s",
+--             mode = { "n", "x", "o" },
+--             function()
+--                 require("flash").jump()
+--             end,
+--             desc = "Flash",
+--         },
+--     },
+-- }
