dcc0f4076ff9041af686f69507b759283d7a195f
vmorganp <31448722+vmorganp@users.noreply.github.com>
2023-10-12 23:37:09 -0700

    better desc

--- kickStartNvim/.config/nvim/lua/custom/plugins/spectre.lua
+++ kickStartNvim/.config/nvim/lua/custom/plugins/spectre.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-pack/nvim-spectre',
     keys = {
-        { "<leader>rr", function() require("spectre").toggle() end, desc = "harpoon add file" },
+        { "<leader>rr", function() require("spectre").toggle() end, desc = "vscodeish find and replace" },
     }
 }
