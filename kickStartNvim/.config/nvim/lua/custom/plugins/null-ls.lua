f91db8d41b8a2468b58471631d2c76d309d3f0fd
vmorganp <31448722+vmorganp@users.noreply.github.com>
2023-10-14 05:39:15 -0700

    add null-ls for actionlint

---
+++ kickStartNvim/.config/nvim/lua/custom/plugins/null-ls.lua
@@ -0,0 +1,16 @@
+return {
+    -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+    "nvimtools/none-ls.nvim",
+    opts = function()
+        local null_ls = require("null-ls")
+        -- local formatting = null_ls.builtins.formatting
+        local diagnostics = null_ls.builtins.diagnostics
+        -- local code_actions = null_ls.builtins.code_actions
+        -- local completion = null_ls.builtins.completion
+        return {
+            sources = {
+                diagnostics.actionlint,
+            },
+        }
+    end,
+}
