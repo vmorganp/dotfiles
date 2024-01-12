ed125644ee9ef162e9837cd33d756802eff07b3e
vmorganp <31448722+vmorganp@users.noreply.github.com>
2024-01-12 01:42:28 -0700

    vim updates

--- kickStartNvim/.config/nvim/lua/custom/plugins/null-ls.lua
+++ kickStartNvim/.config/nvim/lua/custom/plugins/null-ls.lua
@@ -3,13 +3,14 @@ return {
     "nvimtools/none-ls.nvim",
     opts = function()
         local null_ls = require("null-ls")
-        -- local formatting = null_ls.builtins.formatting
+        local formatting = null_ls.builtins.formatting
         local diagnostics = null_ls.builtins.diagnostics
         -- local code_actions = null_ls.builtins.code_actions
         -- local completion = null_ls.builtins.completion
         return {
             sources = {
                 diagnostics.actionlint,
+                formatting.prettier,
             },
         }
     end,
