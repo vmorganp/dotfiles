c681986b9a6e2d5823cd285903fd35b4f6f89cd9
vmorganp <31448722+vmorganp@users.noreply.github.com>
2024-01-17 23:12:03 -0700

    make my laptop usable

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -9,6 +9,7 @@ pkill polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+polybar --reload base &
 MONITOR=DP-0 polybar --reload base &
 # Real cool stuff to run polybar on every monitor which is not what I want
 # # Launch polybar
