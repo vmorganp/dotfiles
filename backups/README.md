483c20e1f967638a67773f6474167ead3c7fff61
vmorganp <31448722+vmorganp@users.noreply.github.com>
2024-06-26 21:30:16 -0700

    stuffnthings

---
+++ backups/README.md
@@ -0,0 +1,9 @@
+Usage:
+
+```
+export AWS_ACCESS_KEY=
+export AWS_SECRET_KEY=
+export AWS_BUCKET=
+export RESTIC_PASSWORD=
+~/dotfiles/backups/backup.sh
+```
--- backups/backup.sh
+++ backups/backup.sh
@@ -1,32 +1,8 @@
+# #!/usr/bin/bash
 # Function to perform backup
 set -e
-unset HISTFILE
 
-check_and_prompt() {
-    local var_name=$1
-
-    if [ -z "${!var_name}" ]; then
-        read -p "$var_name: " input
-        export $var_name=$input
-    fi
-}
-
-# Check and prompt for each environment variable
-check_and_prompt "AWS_ACCESS_KEY"
-check_and_prompt "AWS_SECRET_KEY"
-check_and_prompt "AWS_BUCKET"
-check_and_prompt "RESTIC_PASSWORD"
-
-
-if [[ -z "${DEPLOY_ENV}" ]]; then
-  MY_SCRIPT_VARIABLE="Some default value because DEPLOY_ENV is undefined"
-else
-  MY_SCRIPT_VARIABLE="${DEPLOY_ENV}"
-fi
-
-
-# Set the repository path in AWS S3
-
+. ~/dotfiles/backups/.env
 
 echo "Choose the library you want to back up:"
 echo "1 Work"
@@ -49,13 +25,13 @@ esac
 # Initialize the repo
 # restic -r "s3:https://s3.amazonaws.com/${AWS_BUCKET}/${S3_PATH}"  init
 
-restic -r "s3:https://s3.amazonaws.com/${AWS_BUCKET}/${S3_PATH}"  backup /home/morgan/Documents --exclude-file=/home/morgan/dotfiles/backups/resticignore --dry-run
+restic -r "s3:https://s3.amazonaws.com/${AWS_BUCKET}/${S3_PATH}"  backup /home/morgan/Documents/Projects --exclude-file=/home/morgan/dotfiles/backups/resticignore --dry-run
 
 read -p "Run for real? y/n" run
 
 case $run in
     y)
-        restic -r "s3:https://s3.amazonaws.com/${AWS_BUCKET}/${S3_PATH}"  backup /home/morgan/Documents --exclude-file=/home/morgan/dotfiles/backups/resticignore
+        restic -r "s3:https://s3.amazonaws.com/${AWS_BUCKET}/${S3_PATH}"  backup /home/morgan/Documents/Projects --exclude-file=/home/morgan/dotfiles/backups/resticignore
         ;;
     *)
         echo "Exiting"
